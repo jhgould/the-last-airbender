7c536af0d1d4a1a84f5e2a7cd5d6c6a0a6904eb3
Jack Gould <johnhilsmangould@gmail.com>
2020-07-14 09:55:28 -0600

    add view and api call

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def index
+    tribe = params[:nation].sub(/_/, "+")
+
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/")
+
+    response = conn.get("/api/v1/characters?affiliation=#{tribe}") do |faraday|
+      faraday.headers
+    end
+    @json = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+end
