75510b035f55018541055f75f42d7f8fbec92819
Jack Gould <johnhilsmangould@gmail.com>
2020-07-14 10:48:51 -0600

    cleaned up AirbenderApi class

--- app/services/airbender_api.rb
+++ app/services/airbender_api.rb
@@ -5,7 +5,7 @@ class AirbenderApi
     response = conn.get("/api/v1/characters?affiliation=#{tribe}") do |faraday|
       faraday.headers
     end
-    json = JSON.parse(response.body, symbolize_names: true)
+     JSON.parse(response.body, symbolize_names: true)
   end
 
 end
