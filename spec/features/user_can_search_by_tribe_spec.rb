e506026b838ccf4149672f5b947df99e08da533c
Jack Gould <johnhilsmangould@gmail.com>
2020-07-14 09:31:50 -0600

    add basics of setup

---
+++ spec/features/user_can_search_by_tribe_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+
+RSpec.describe "User can seach for avatars by tribe" do
+  it "user searches by Fire Nation" do
+    visit '/'
+    select 
+
+  end
+
+end
+
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members“
+# Then I should be on page “/search”
+# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
+# And I should see a list with the detailed information for the 20 members of the Fire Nation.
+#
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies and a list of enemies
+# - Any affiliation that the member has
