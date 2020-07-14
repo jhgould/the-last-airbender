4cc4bdd0ccfb35c64c3fb39260275e6c4da23b57
Jack Gould <johnhilsmangould@gmail.com>
2020-07-14 09:35:59 -0600

    add test

--- spec/features/user_can_search_by_tribe_spec.rb
+++ spec/features/user_can_search_by_tribe_spec.rb
@@ -4,20 +4,20 @@ require 'rails_helper'
 RSpec.describe "User can seach for avatars by tribe" do
   it "user searches by Fire Nation" do
     visit '/'
-    select 
+    select "Fire Nation", from: :nation
+    click_on "Search For Members"
+    expect(current_path).to eq('/search')
+    expect(page).to have_content("20 People")
+
+    within(first(".memeber")) do
+      expect(page).to have_css(".name")
+    end
 
   end
 
 end
 
 
-# As a user,
-# When I visit "/"
-# And I Select "Fire Nation" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members“
-# Then I should be on page “/search”
-# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
 # And I should see a list with the detailed information for the 20 members of the Fire Nation.
 #
 # And for each of the members I should see:
